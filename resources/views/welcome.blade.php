f7263450228c748059afda9e94427100fcbbe0a5
我 白菜 <66418500@qq.com>
2020-01-02 11:42:01 +0800

    钩子测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    学无止境
+                    学无止境啦啦啦
                 </div>
 
                 <div class="links">
