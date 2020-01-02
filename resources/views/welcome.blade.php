95cb11f48474b6f96c95f5786da6a5352cf2a6bb
我 白菜 <66418500@qq.com>
2020-01-02 11:27:07 +0800

    调试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    黑暗森林
+                    学无止境
                 </div>
 
                 <div class="links">
